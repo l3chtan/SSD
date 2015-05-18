454e7c9184e521049de0de3708f2e62014c2cee5
Edward Toth <e0725631@student.tuwien.ac.at>
2015-05-18 16:13:51 +0200

    removed unused code and added comments

--- UE3/ssd-ss15-exercise3/src/ssd/JeopardyMoveHandler.java
+++ UE3/ssd-ss15-exercise3/src/ssd/JeopardyMoveHandler.java
@@ -79,18 +79,6 @@ public class JeopardyMoveHandler extends DefaultHandler {
 	//Specify additional methods to parse the move document and modify the jeopardyDoc
 
 	@Override
-	public void startDocument(){
-
-
-		System.out.println("Beginning of document");
-	}
-
-	@Override
-	public void endDocument(){
-		System.out.println("End of document");
-	}
-
-	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) {
 
 		NodeList nl = null;
@@ -99,14 +87,7 @@ public class JeopardyMoveHandler extends DefaultHandler {
 			nl = getNodeList("//game[@session=\""+session+"\"]");
 
 			game = nl.item(0);
-
-
-		}/* else if(qName.equals("question")){jeopardy/games/game
-			System.out.println(session);
-			nl = getNodeList("jeopardy/games/game/asked[@question=\""+eleText+"\"]");
-		//	asked = getNode("asked","question",attributes);
-			System.out.println(nl.getLength());
-		}*/
+		}
 	}
 
 	@Override
@@ -114,19 +95,19 @@ public class JeopardyMoveHandler extends DefaultHandler {
 		Element elem = null;
 		NodeList lst = null;
 		if(qName.equals("player")){
-		//	lst = getNodeList("jeopardy/games/game[@session=\""+session+"\"]/player");
 
+			/*add a new player node to the game, if there are less than two players in the game*/
 			if(getNodeList("//game[@session=\""+session+"\"]/player").getLength() < 2) {
 				player = eleText;
 				elem = newElem("player", "ref");
-				//	game.appendChild(elem);
 				game.insertBefore(elem, game.getChildNodes().item(0));
 			}
+			/*check if there is already an "asked" element with the same question number.
+			 *If there is, add the question to that node, create a new asked element otherwise*/
 		} else if(qName.equals("question")){
 			String path = "//game[@session=\""+session+"\"]/asked[@question=\""+eleText+"\"]";
 			lst = getNodeList(path);
 			if(lst.getLength() < 1) {
-				System.out.println(lst.getLength());
 
 				elem = newElem("asked", "question");
 				elem.setAttribute("question", eleText);
@@ -134,9 +115,7 @@ public class JeopardyMoveHandler extends DefaultHandler {
 				lst = getNodeList(path);
 			}
 				asked = lst.item(0);
-				System.out.println("foo");
-
-
+			/*add an answer of the player to the list of given answers*/
 		} else if(qName.equals("answer")){
 			elem = newElem("givenanswer", "player");
 			elem.setAttribute("player", player);
@@ -151,6 +130,7 @@ public class JeopardyMoveHandler extends DefaultHandler {
 		return elem;
 	}
 
+	/*get a list of nodes specifid in "path"*/
 	private NodeList getNodeList(String path){
 		NodeList gameList = null;
 		try {
@@ -162,19 +142,5 @@ public class JeopardyMoveHandler extends DefaultHandler {
 		}
 		return gameList;
 	}
-
-/*	private Node getNode(String node, String attr, Attributes attributes){
-		Node temp = null;
-		for (int i = 0; i < jeopardyDoc.getElementsByTagName(node).getLength(); i++) {
-			temp = jeopardyDoc.getElementsByTagName(node).item(i);
-			if (temp.getAttributes().getNamedItem(attr).getNodeValue()
-					.equals(attributes.getValue(attr))) {
-				System.out.println("jeopardy: " + temp.getAttributes().getNamedItem(attr).getNodeValue());
-				System.out.println("move: " + attributes.getValue(attr));
-				return temp;
-			}
-		}
-		return temp;
-	}*/
 }
 
